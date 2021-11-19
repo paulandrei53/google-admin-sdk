ea463f4fab01db6e18452032e86302bb99b760e7
Paul Andrei <paulandrei.7am@gmail.com>
2021-11-19 13:42:38 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    Batch: require("./lib/batch"),
+    AdminSDK: require("./lib/google_api_admin_sdk"),
+    GroupProvisioning: require("./lib/group_provisioning"),
+    OrgUnitProvisioning: require("./lib/org_unit_provisioning"),
+    UserProvisioning: require("./lib/user_provisioning"),
+    GoogleQuery: require("./lib/query")
+};
